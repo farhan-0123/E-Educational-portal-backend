fefe61a210406bc4c83661ee8d6ca66093648396
farhan-0123 <farhanmemon0215@gmail.com>
2022-02-28 11:23:40 +0530

    schedule:
    Exam Questions get deleted every monday

--- api_interface/schedular.py
+++ api_interface/schedular.py
@@ -1,5 +1,5 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from .models import Chat
+from .models import Chat, ExamQuestion
 from django.utils import timezone
 
 
@@ -9,8 +9,15 @@ def chat_delete():
     Chat.objects.all().delete()
 
 
+def exam_delete():
+    from datetime import datetime
+    print("SCHEDULER Deleting the Exam...", datetime.now())
+    ExamQuestion.objects.all().delete()
+
+
 def start_scheduler():
     print("Starting the background scheduler...")
     scheduler = BackgroundScheduler(timezone=timezone.get_current_timezone_name())
     scheduler.add_job(chat_delete, 'interval', days=1)
+    scheduler.add_job(exam_delete, 'cron', day_of_week='mon')
     scheduler.start()
